21105c4d2e9b223381847447b145b4175afa1c11
yl <78755@qq.com>
2018-11-21 23:50:51 +0800

    轮播图效果初步完成

--- my_project/js/index.js
+++ my_project/js/index.js
@@ -1,11 +1,10 @@
 $(function(){
   //轮播图定时器
-  var i = 1;
+  var i = 0;
   var flag = 0;
   function change() {
-      if(i==4){
+      if(i==3){
         flag = 1;
-        i=3;
       }
       if(i==0){
         flag = 0;
@@ -16,32 +15,46 @@ $(function(){
           .siblings().removeClass("active");
         $("#nav ul").css("marginLeft",`-${i*1140}px`);
       }else{
+        i++;
         $($(".l1")[i]).addClass("active")
           .siblings().removeClass("active");
         $("#nav ul").css("marginLeft",`-${i*1140}px`);
-        i++;
       }
-
   }
   var timer=setInterval(change,3000);
 
   //轮播图当鼠标放入时，停止轮播
-    $("#nav>ul").mouseenter(function(){
-        clearInterval(timer);
-    });
-    //鼠标离开，继续轮播
-    $("#nav>ul").mouseleave(function(){
-        timer=setInterval(change,3000);
-    })
+  $(".banner").mouseenter(function(){
+      clearInterval(timer);
+  });
+  //鼠标离开，继续轮播
+  $(".banner").mouseleave(function(){
+      timer=setInterval(change,3000);
+  })
 
-    //点击底部圆点时显示对应的图片
-    $(".banner_bo>ul").on("click","li",function(){
-        console.log(11111111);
-        var $li=$(this);
-        //获取id的值找到对应的图片中data-target属性值
-        var value="#"+$li.attr("id");
-        console.log(value);
+  //点击底部圆点时显示对应的图片
+  $(".banner_bo>ul").on("click","li",function(){
+      var $li=$(this);
+      //获取id的值找到对应的图片中data-target属性值
+      var value=$li.attr("id");
+      var i=$("#nav>ul a").index($(`#nav>ul a[data-target$=${value}]`));
+      $("#nav>ul").css("marginLeft",`-${i*1140}px`);
+      $li.addClass("active")
+        .siblings().removeClass("active");
+  })
 
-        console.log($(`"#nav>ul a[data-target=${value}]"`))
+  //点击左右箭头按钮切换图片
+    $("div.left>a").on("click", function (e) {
+      e.preventDefault();
+      if(i<3) {
+        $(".banner_bo li").eq(++i).click();
+      }
     })
+  $("div.right>a").on("click", function (e) {
+    e.preventDefault();
+    if(i>0) {
+      $(".banner_bo li").eq(--i).click();
+    }
+  })
+
 });
